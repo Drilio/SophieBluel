34b79b8d7a4198fc8e5eebbe17f766234c99c3d1
Antoine ROY <antoineroy92@gmail.com>
2023-03-01 10:46:42 +0100

    works fonctionnel, filters fonctionnel !

--- filters.js
+++ filters.js
@@ -2,34 +2,32 @@
 const reponse = await fetch("http://localhost:5678/api/works");
 const filters = await reponse.json();
 
-function extractFiltre(filters){
-    for (let i = 0; i < filters.length; i++) {
-    
-        const article = filters[i];
-        const arrayFilters = [];
-        const buttonElement = article.category.name
-        arrayFilters.push(buttonElement);
-        const setFilters = new Set();
-        setFilters.add(arrayFilters[i]);
-        console.log(setFilters);
-}
-}
-
 function genererFilters(filters){
+    // on fait un tableau contenant uniquement les noms qu'on veut utiliser comme filtres à partir du JSON 
+    const arrayFilters = []
+    for (let i = 0; i < filters.length; i++){
+        const article = filters[i];
+        arrayFilters.push(article.category.name);     
+        // console.log(arrayFilters);   
+    }
+    
+    // on retire les doublons du tableau arrayFilters
+    const setFilters = new Set(arrayFilters); 
+    const usefullFilters = Array.from(setFilters);
+    console.log(usefullFilters);
 
-        for (let i = 0; i < filters.length; i++){
-            const article = filters[i];
+    for (let i = 0; i < usefullFilters.length; i++){
+
             // Récupération de l'élément du DOM accueillant les contenus générés
             const sectionGallery = document.querySelector(".filters");
             // Création d’une balise dédiée à un contenu
             const filtersElement = document.createElement("button");
-            filtersElement.innerText = article.category.name;
+            //on itere le contenu de notre html avec le tableau usefullFilters débarassé des doublons
+            filtersElement.innerText = usefullFilters[i];
            
             // On rattache les balises à la section gallery
             sectionGallery.appendChild(filtersElement);
         }
-
     }
 
-extractFiltre(filters);
 genererFilters(filters);
